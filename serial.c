2d0bd1d379329d13b3df02bdbd48285b165b301e
Almir Neto <almirneto338@gmail.com>
2025-03-27 14:29:15 -0300

    feat: adding argument reading from stdin feat: adding output as image

--- serial.c
+++ serial.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#include "png.h"
 
 #include "common.h"
 
 int main(int argc, char *argv[])
 {
 
-    char arguments[256];
+    int width, height, maxIterations;
+    double xMin, xMax, yMin, yMax;
 
-    if (fgets(arguments, sizeof(arguments), stdin) != NULL)
-    {
-        printf("-> Arguments: %s\n", arguments);
-    }
+    scanf("%d %d %lf %lf %lf %lf %d", &width, &height, &xMin, &xMax, &yMin, &yMax, &maxIterations);
 
-    int width = 7680, height = 4320;
+    printf("-> Starting mandelbrot generation\n");
+    printf("-> Resolution: %dx%d\n", width, height);
+    printf("-> Complex plane limit (x): [%lf, %lf]\n", xMin, xMax);
+    printf("-> Complex plane limit (y): [%lf, %lf]\n", yMin, yMax);
+    printf("-> Max number of iterations: %d\n", maxIterations);
 
-    double *space = create_2d_linear_space(100, 100);
+    int *space = malloc(sizeof(int) * width * height);
 
-    double *xLinearSpace = create_linear_space(-5.0, 5.0, width);
-    double *yLinerSpace = create_linear_space(-5.0, 5.0, height);
+    double *xLinearSpace = create_linear_space(xMin, xMax, width);
+    double *yLinearSpace = create_linear_space(yMin, yMax, height);
 
     complex z, c;
     int iteration = 0;
-    int max_iter = 1000000;
-
-    double real, imag;
-    double a, b;
 
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
         {
 
-            z.real = 0;
-            z.imag = 0;
             iteration = 0;
 
+            z.real = 0;
+            z.imag = 0;
+
             c.real = xLinearSpace[i];
-            c.imag = yLinerSpace[j];
-            while (complex_modulus(z) < 2.0 && iteration < max_iter)
+            c.imag = yLinearSpace[j];
+
+            while (iteration < maxIterations && abs_complex(z) <= 2)
             {
-                real = z.real * z.real - z.imag * z.imag;
-                imag = z.real * z.imag + z.imag * z.real;
-                z.real = real + c.real;
-                z.imag = imag + c.imag;
+
+                z = complex_squared(z);
+
+                z.real = z.real + c.real;
+                z.imag = z.imag + c.imag;
+
                 iteration++;
             }
+
+            space[j + i * height] = iteration;
         }
     }
 
@@ -54,11 +61,58 @@ int main(int argc, char *argv[])
     if (!mandelbrot)
     {
         printf("Unable to open output file\n");
+        return -1;
     }
+
+    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png)
+    {
+        fclose(mandelbrot);
+        return -1;
+    }
+
+    png_infop info = png_create_info_struct(png);
+    if (!info)
+    {
+        png_destroy_write_struct(&png, NULL);
+        fclose(mandelbrot);
+        return -1;
+    }
+
+    if (setjmp(png_jmpbuf(png)))
+    {
+        png_destroy_write_struct(&png, &info);
+        fclose(mandelbrot);
+        return -1;
+    }
+
+    png_set_compression_level(png, 0);
+    png_init_io(png, mandelbrot);
+    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png, info);
+
+    png_bytep row = (png_bytep)malloc(3 * width * sizeof(png_byte));
+    int color = 0;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            color = (int)(((double)space[x * height + y] / maxIterations) * 255);
+            row[x * 3] = color;     // Red
+            row[x * 3 + 1] = color; // Green
+            row[x * 3 + 2] = color; // Blue
+        }
+        png_write_row(png, row);
+    }
+    free(row);
+
+    png_write_end(png, NULL);
+    png_destroy_write_struct(&png, &info);
+    fclose(mandelbrot);
 
     free(space);
     free(xLinearSpace);
-    free(yLinerSpace);
+    free(yLinearSpace);
 
     return 0;
 }
