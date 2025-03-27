cfd3e05c4e219dac3630970e8710c131031927fc
Almir Neto <almirneto338@gmail.com>
2025-03-27 09:35:49 -0300

    feat: adding mandelbrot main code

--- serial.c
+++ serial.c
@@ -3,16 +3,55 @@
 
 #include "common.h"
 
-int main()
+int main(int argn, char **argv)
 {
 
-    complex c;
+    int width = 7680, height = 4320;
 
-    c.real = 5.4;
-    c.imag = 8.0;
+    double *space = create_2d_linear_space(100, 100);
 
-    printf("-> Real: %lf <-> Imag: %lf\n", c.real, c.imag);
-    printf("-> Absolute value: %lf\n", complex_absolute_value(c));
+    double *xLinearSpace = create_linear_space(-5.0, 5.0, width);
+    double *yLinerSpace = create_linear_space(-5.0, 5.0, height);
+
+    complex z, c;
+    int iteration = 0;
+    int max_iter = 1000000;
+
+    double real, imag;
+    double a, b;
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+
+            z.real = 0;
+            z.imag = 0;
+            iteration = 0;
+
+            c.real = xLinearSpace[i];
+            c.imag = yLinerSpace[j];
+            while (complex_modulus(z) < 2.0 && iteration < max_iter)
+            {
+                real = z.real * z.real - z.imag * z.imag;
+                imag = z.real * z.imag + z.imag * z.real;
+                z.real = real + c.real;
+                z.imag = imag + c.imag;
+                iteration++;
+            }
+        }
+    }
+
+    FILE *mandelbrot = fopen("mandelbrot.png", "wb");
+
+    if (!mandelbrot)
+    {
+        printf("Unable to open output file\n");
+    }
+
+    free(space);
+    free(xLinearSpace);
+    free(yLinerSpace);
 
     return 0;
 }
